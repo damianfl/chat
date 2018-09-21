52797fa9ba8c0c60ed62e5964078343e41fd7dab
damianfl <damian87fl@gmail.com>
2018-09-21 19:46:28 +0200

    api removed

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 var config = {
-  apiKey: "AIzaSyB3J3PCX0nWnVCY13Qwr-YJOo3P_0v571g",
-  authDomain: "new-chat-8af61.firebaseapp.com",
-  databaseURL: "https://new-chat-8af61.firebaseio.com",
-  projectId: "new-chat-8af61",
-  storageBucket: "new-chat-8af61.appspot.com",
-  messagingSenderId: "555048126314"
+  //your firebase api key
 };
 const fire = firebase.initializeApp(config);
 export default fire;

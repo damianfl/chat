18707155903d86948f0fe0e872e47e90d146aa9a
damianfl <damian87fl@gmail.com>
2018-09-21 16:47:44 +0200

    some changes

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 var config = {
-  apiKey: "AIzaSyB3J3PCX0nWnVCY13Qwr-YJOo3P_0v571g",
-  authDomain: "new-chat-8af61.firebaseapp.com",
-  databaseURL: "https://new-chat-8af61.firebaseio.com",
-  projectId: "new-chat-8af61",
-  storageBucket: "new-chat-8af61.appspot.com",
-  messagingSenderId: "555048126314"
+  //Your firebase config
 };
 const fire = firebase.initializeApp(config);
 export default fire;
